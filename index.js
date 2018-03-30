6f5aa5f5e423c509ca6860ab2ed005ba2b3f1d9e
fmvilas <fmvilas@gmail.com>
2018-03-30 13:09:55 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,4 @@
+module.exports = {
+  '1.0.0': require('./schemas/1.0.0.json'),
+  '1.1.0': require('./schemas/1.1.0.json'),
+};
---
+++ test/index.js
@@ -0,0 +1,32 @@
+const assert = require('assert');
+
+describe('AsyncAPI', () => {
+  it('should return an object', () => {
+    const asyncapi = require('..');
+    assert(typeof asyncapi === 'object', 'Returned value is not an object.');
+  });
+
+  // Version 1.1.0
+
+  it('should return an object with 1.1.0 key', () => {
+    const asyncapi = require('..');
+    assert(typeof asyncapi['1.1.0'] === 'object', 'Returned object does not contain 1.1.0.');
+  });
+  it('should return schema version 1.1.0', () => {
+    const asyncapi = require('..')['1.1.0'];
+    const asyncapi100 = require('../schemas/1.1.0.json');
+    assert.deepStrictEqual(asyncapi, asyncapi100, 'Returned object is not schema version 1.1.0.');
+  });
+
+  // Version 1.0.0
+
+  it('should return an object with 1.0.0 key', () => {
+    const asyncapi = require('..');
+    assert(typeof asyncapi['1.0.0'] === 'object', 'Returned object does not contain 1.0.0.');
+  });
+  it('should return schema version 1.0.0', () => {
+    const asyncapi = require('..')['1.0.0'];
+    const asyncapi100 = require('../schemas/1.0.0.json');
+    assert.deepStrictEqual(asyncapi, asyncapi100, 'Returned object is not schema version 1.0.0.');
+  });
+});
