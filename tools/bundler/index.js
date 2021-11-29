462592ef06fa63f0968be1a6798bb91d4669a94e
Jonas Lagoni <jonas-lt@live.dk>
2021-11-29 07:59:37 +0100

    Correct bundling behavior

--- tools/bundler/index.js
+++ tools/bundler/index.js
@@ -3,6 +3,7 @@ const path = require('path');
 const fs = require('fs');
 const definitionsDirectory = path.resolve(__dirname, '../../definitions');
 const outputDirectory = path.resolve(__dirname, '../../schemas');
+const Bundler = require("@hyperjump/json-schema-bundle");
 console.log(`Looking for separate definitions in the following directory: ${definitionsDirectory}`);
 console.log(`Using the following output directory: ${outputDirectory}`);
 
@@ -12,6 +13,30 @@ console.log(`Using the following output directory: ${outputDirectory}`);
 (async () => {
   const versions = await fs.promises.readdir(definitionsDirectory);
   console.log(`The following versions have separate definitions: ${versions.join(',')}`);
+  for (const version of versions) {
+    console.log(`Bundling the following version together: ${version}`);
+    const versionDir = path.resolve(definitionsDirectory, version);
+    const definitions = await fs.promises.readdir(versionDir);
+    const definitionFiles = definitions.map((file) => fs.readFileSync(path.resolve(versionDir, file)));
+    const definitionJson = definitionFiles.map((file) => JSON.parse(file));
+
+    for (const jsonFile of definitionJson) {
+      Bundler.add(jsonFile);
+    }
+    console.log(definitionJson); 
+    const fileToBundle = `file://${definitionsDirectory}/${version}/asyncapi.json`;
+    
+    const main = await Bundler.get(fileToBundle);
+    const bundle = await Bundler.bundle(main);
+    bundle.description = `!!Auto generated!! \n Do not manually edit. ${bundle.description ?? ''}`;
+    const outputFile = path.resolve(outputDirectory, `${version}.json`);
+    console.log(`Writing the bundled file to: ${outputFile}`);
+    fs.promises.writeFile(outputFile, JSON.stringify(bundle, null, 4));
+  }
+  console.log('done');
+})();
+
+(async () => {
   for (const version of versions) {
     console.log(`Bundling the following version together: ${version}`);
     let schema = await $RefParser.bundle(`file://${definitionsDirectory}/${version}/asyncapi.json`, {path: definitionsDirectory});
