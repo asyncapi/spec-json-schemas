6a9995f83c3d65e4c8e5dec02b514b89b44d2d70
Jonas Lagoni <jonas-lt@live.dk>
2021-12-07 13:22:38 +0100

    added try catch

--- tools/bundler/index.js
+++ tools/bundler/index.js
@@ -1,4 +1,3 @@
-const $RefParser = require("@apidevtools/json-schema-ref-parser");
 const path = require('path');
 const fs = require('fs');
 const definitionsDirectory = path.resolve(__dirname, '../../definitions');
@@ -14,24 +13,28 @@ console.log(`Using the following output directory: ${outputDirectory}`);
   const versions = await fs.promises.readdir(definitionsDirectory);
   console.log(`The following versions have separate definitions: ${versions.join(',')}`);
   for (const version of versions) {
-    console.log(`Bundling the following version together: ${version}`);
-    const versionDir = path.resolve(definitionsDirectory, version);
-    const definitions = await fs.promises.readdir(versionDir);
-    const definitionFiles = definitions.map((file) => fs.readFileSync(path.resolve(versionDir, file)));
-    const definitionJson = definitionFiles.map((file) => JSON.parse(file));
+    try{
+      console.log(`Bundling the following version together: ${version}`);
+      const versionDir = path.resolve(definitionsDirectory, version);
+      const definitions = await fs.promises.readdir(versionDir);
+      const definitionFiles = definitions.filter((value) => {return !value.includes('asyncapi')}).map((file) => fs.readFileSync(path.resolve(versionDir, file)));
+      const definitionJson = definitionFiles.map((file) => JSON.parse(file));
 
-    for (const jsonFile of definitionJson) {
-      Bundler.add(jsonFile);
+      for (const jsonFile of definitionJson) {
+        Bundler.add(jsonFile);
+      }
+      console.log(definitionJson); 
+      const fileToBundle = `file://${definitionsDirectory}/${version}/asyncapi.json`;
+      
+      const main = await Bundler.get(fileToBundle);
+      const bundledSchema = await Bundler.bundle(main);
+      bundledSchema.description = `!!Auto generated!! \n Do not manually edit. ${bundledSchema.description ?? ''}`;
+      const outputFile = path.resolve(outputDirectory, `${version}.json`);
+      console.log(`Writing the bundled file to: ${outputFile}`);
+      await fs.promises.writeFile(outputFile, JSON.stringify(bundledSchema, null, 4));
+    }catch(e) {
+      console.log(e);
     }
-    console.log(definitionJson); 
-    const fileToBundle = `file://${definitionsDirectory}/${version}/asyncapi.json`;
-    
-    const main = await Bundler.get(fileToBundle);
-    const bundle = await Bundler.bundle(main);
-    bundle.description = `!!Auto generated!! \n Do not manually edit. ${bundle.description ?? ''}`;
-    const outputFile = path.resolve(outputDirectory, `${version}.json`);
-    console.log(`Writing the bundled file to: ${outputFile}`);
-    fs.promises.writeFile(outputFile, JSON.stringify(bundle, null, 4));
   }
   console.log('done');
 })();
