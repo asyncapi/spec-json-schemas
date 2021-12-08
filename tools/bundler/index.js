bc1d7ca139dbc31dfe104966e081371f680d3f77
Jonas Lagoni <jonas-lt@live.dk>
2021-12-08 15:43:08 +0100

    Added proper ids and references

--- tools/bundler/index.js
+++ tools/bundler/index.js
@@ -19,14 +19,12 @@ console.log(`Using the following output directory: ${outputDirectory}`);
       const definitions = await fs.promises.readdir(versionDir);
       const definitionFiles = definitions.filter((value) => {return !value.includes('asyncapi')}).map((file) => fs.readFileSync(path.resolve(versionDir, file)));
       const definitionJson = definitionFiles.map((file) => JSON.parse(file));
-
       for (const jsonFile of definitionJson) {
-        Bundler.add(jsonFile, `file://${versionDir}/${jsonFile.$id}.json`);
+        Bundler.add(jsonFile);
       }
-      console.log(definitionJson); 
-      const fileToBundle = `file://${versionDir}/asyncapi.json`;
-      const main = await Bundler.get(fileToBundle);
-      const bundledSchema = await Bundler.bundle(main);
+      const filePathToBundle = `file://${versionDir}/asyncapi.json`;
+      const fileToBundle = await Bundler.get(filePathToBundle);
+      const bundledSchema = await Bundler.bundle(fileToBundle);
       bundledSchema.description = `!!Auto generated!! \n Do not manually edit. ${bundledSchema.description ?? ''}`;
       const outputFile = path.resolve(outputDirectory, `${version}.json`);
       console.log(`Writing the bundled file to: ${outputFile}`);
