126582ddd1cc6c28065f5e3f8e0363d5af78017b
Jonas Lagoni <jonas-lt@live.dk>
2021-12-09 19:50:16 +0100

    Fixed tests

--- test/index.js
+++ test/index.js
@@ -10,9 +10,7 @@ describe('AsyncAPI', () => {
 
   it('should check if json schema is exported and if it matches the original file', () => {
     const files = fs.readdirSync('schemas');
-    files.filter((file) => {
-      return !fs.lstatSync(path.resolve(__dirname, `../schemas/${file}`)).isDirectory();
-    }).forEach(file => {
+    files.forEach(file => {
       const fileName = path.parse(file).name;
       
       const asyncapi = require('..');
