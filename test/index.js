ac64e4a0bb404dd49de8c8be2b203d9e204ea2d6
Matatjahu <urbanczyk.maciej.95@gmail.com>
2022-09-30 14:54:59 +0200

    fix tests

--- test/index.js
+++ test/index.js
@@ -9,7 +9,7 @@ describe('AsyncAPI', () => {
   });
 
   it('should check if json schema is exported and if it matches the original file', () => {
-    const skipFiles = ["README", "all.schema-store"];
+    const skipFiles = ["README", "all.schema-store", "1.0.0", "1.1.0", "1.2.0", "2.0.0-rc1", "2.0.0-rc2"];
     const files = fs.readdirSync('schemas');
     files.forEach(file => {
       const fileName = path.parse(file).name;
