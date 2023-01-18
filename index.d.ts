32dbed811cafc0862c8f214e5793547a574e1df4
Lukasz Gornicki <lpgornicki@gmail.com>
2023-01-18 09:05:23 +0100

    chore: update `next-spec` with latest `master` (#301)

---
+++ index.d.ts
@@ -0,0 +1,11 @@
+import type { JSONSchema7 } from 'json-schema';
+
+declare const _exports: {
+    '2.0.0': JSONSchema7;
+    '2.1.0': JSONSchema7;
+    '2.2.0': JSONSchema7;
+    '2.3.0': JSONSchema7;
+    '2.4.0': JSONSchema7;
+    '2.5.0': JSONSchema7;
+};
+export = _exports;
