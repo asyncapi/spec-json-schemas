26acf98e63c8013ebdbd69ad70d60aa576a50187
Jonas Lagoni <jonas-lt@live.dk>
2023-03-13 13:37:28 +0100

    test: increase timeout that periodically fails (#350)

--- test/schemas.js
+++ test/schemas.js
@@ -7,27 +7,29 @@ const versionsToTest = [
     version: '3.0.0'
   }
 ]
-
-it('Versions should accurately validate documents', () => {
-  for (const versionToTest of versionsToTest) {
-    const version = versionToTest.version;
-    const asyncapi = require('..');
-    const schema = asyncapi[version];
-    delete schema.definitions['http://json-schema.org/draft-07/schema'];
-    const ajv = new Ajv({
-      jsonPointers: true,
-      allErrors: true,
-      schemaId: '$id',
-      logger: false,
-      validateFormats: false,
-      strict: false
-    });
-    const documentPaths = fs.readdirSync(path.resolve(__dirname, `./docs/${version}`)).map((pathToDoc) => {return path.resolve(__dirname, `./docs/${version}/${pathToDoc}`)});
-    for (const documentPath of documentPaths) {
-      const document = require(documentPath);
-      const validate = ajv.compile(schema)
-      const valid = validate(document)
-      assert(valid === true, 'Document must be validated correctly: ' + JSON.stringify(validate.errors, null, 4));
+describe("Should be able to validate", function () {
+  this.timeout(30000); 
+  it('all valid documents', () => {
+    for (const versionToTest of versionsToTest) {
+      const version = versionToTest.version;
+      const asyncapi = require('..');
+      const schema = asyncapi[version];
+      delete schema.definitions['http://json-schema.org/draft-07/schema'];
+      const ajv = new Ajv({
+        jsonPointers: true,
+        allErrors: true,
+        schemaId: '$id',
+        logger: false,
+        validateFormats: false,
+        strict: false
+      });
+      const documentPaths = fs.readdirSync(path.resolve(__dirname, `./docs/${version}`)).map((pathToDoc) => {return path.resolve(__dirname, `./docs/${version}/${pathToDoc}`)});
+      for (const documentPath of documentPaths) {
+        const document = require(documentPath);
+        const validate = ajv.compile(schema)
+        const valid = validate(document)
+        assert(valid === true, 'Document must be validated correctly: ' + JSON.stringify(validate.errors, null, 4));
+      }
     }
-  }
+  });
 });
