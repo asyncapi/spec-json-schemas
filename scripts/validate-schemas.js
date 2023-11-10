713792d96e29f9418395eda00ac3c17ef4ccc974
AnimeshKumar923 <kumaranimesh923@gmail.com>
2023-11-11 02:26:05 +0530

    fix: improve code, add more outputs

--- scripts/validate-schemas.js
+++ scripts/validate-schemas.js
@@ -1,21 +1,9 @@
 const Ajv = require('ajv');
-const fs = require('fs');
+const fs = require('fs').promises;
 const path = require('path');
 const { Parser } = require('@asyncapi/parser');
 
-// Specify the folder where your JSON Schema document is located
-// const schemaFolderPath = path.join(__dirname, '../schemas'); // Replace 'schemas' with your folder name
 
-// Specify the name of your JSON Schema file
-// const schemaFileName = '2.6.0.json'; // Replace 'your-schema.json' with your actual file name
-
-// Construct the full path to the JSON Schema file
-// const schemaFilePath = path.join(schemaFolderPath, schemaFileName);
-
-// Load your JSON Schema document
-const schemaDocument = fs.readFileSync('./schemas/2.6.0.json', 'utf-8');
-
-// const schema = JSON.parse(schemaDocument);
 
 
 
@@ -24,6 +12,28 @@ async function parsing(){
   try {
     // Start measuring time
     console.time('Parsing time');
+
+
+
+    // Specify the folder where your JSON Schema document is located
+    const schemaFolderPath = path.join(__dirname, '../schemas'); // Replace 'schemas' with your folder name
+
+    // Specify the name of your JSON Schema file
+    const schemaFileName = '2.6.0.json'; // Replace 'your-schema.json' with your actual file name
+
+    // Construct the full path to the JSON Schema file
+    const schemaFilePath = path.join(schemaFolderPath, schemaFileName);
+
+    // Load your JSON Schema document
+    const schemaDocument = await fs.readFile (schemaFilePath, 'utf-8');
+
+    // Log the length of the schemaDocument to help diagnose if it's reading the file properly
+    console.log('Length of schemaDocument:', schemaDocument.length);
+
+    // const schema = JSON.parse(schemaDocument);
+
+
+
 
     const parser = new Parser();
     const { document } = await parser.parse(schemaDocument);
@@ -51,7 +61,7 @@ async function parsing(){
 
     // Stop measuring time
     console.timeEnd('Parsing time');
-    
+
   } catch (error) {
     console.error('Error parsing AsyncAPI document:', error.message);
   }
