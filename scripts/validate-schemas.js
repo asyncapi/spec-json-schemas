5471112e6fb117ba174ebd9aa166e3b0d8ccd443
AnimeshKumar923 <kumaranimesh923@gmail.com>
2023-11-11 02:32:06 +0530

    fix: improve code, remove comments

--- scripts/validate-schemas.js
+++ scripts/validate-schemas.js
@@ -4,34 +4,18 @@ const path = require('path');
 const { Parser } = require('@asyncapi/parser');
 
 
-
-
-
-
 async function parsing(){
   try {
     // Start measuring time
     console.time('Parsing time');
 
-
-
-    // Specify the folder where your JSON Schema document is located
-    const schemaFolderPath = path.join(__dirname, '../schemas'); // Replace 'schemas' with your folder name
-
-    // Specify the name of your JSON Schema file
-    const schemaFileName = '2.6.0.json'; // Replace 'your-schema.json' with your actual file name
-
-    // Construct the full path to the JSON Schema file
-    const schemaFilePath = path.join(schemaFolderPath, schemaFileName);
-
     // Load your JSON Schema document
-    const schemaDocument = await fs.readFile (schemaFilePath, 'utf-8');
+    const schemaDocument = await fs.readFile ('./schemas/2.6.0.json', 'utf-8');
 
     // Log the length of the schemaDocument to help diagnose if it's reading the file properly
     console.log('Length of schemaDocument:', schemaDocument.length);
 
     // const schema = JSON.parse(schemaDocument);
-
 
 
 
@@ -68,10 +52,4 @@ async function parsing(){
 }
 
 parsing();
-
-
-
-
-
-
 
