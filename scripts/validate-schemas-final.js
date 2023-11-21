d584bb000dbe7aff7dede2677ffbf4978ffe4144
Animesh Kumar <kumaranimesh923@gmail.com>
2023-11-21 21:38:40 +0530

    chore: rename files, update dependencies
    Changes:
    
    - updated the dependencies
    - rename config.json file
    - separete the validate-schema file into two versions

---
+++ scripts/validate-schemas-final.js
@@ -0,0 +1,99 @@
+const fs = require('fs');
+const path = require('path');
+
+
+function draft04(){
+   const Ajv = require("ajv-draft-04");
+   const ajv = new Ajv();
+
+
+   // Specify the path to the 'schemas' directory
+   const directoryPath = '../schemas';
+
+   // var json = globToRegExp("*.json");
+
+   // Read the files from the 'schemas' directory
+
+   fs.readdirSync(directoryPath).forEach(file => {
+   // Construct the full path to the JSON schema file
+   const filePath = path.join(directoryPath, file);
+
+   try {
+      // Read and parse the JSON schema
+      const fileContent = fs.readFileSync(filePath, 'utf8');
+      const obj = JSON.parse(fileContent);
+      
+      delete obj.definitions['http://json-schema.org/draft-04/schema'];
+      delete obj.definitions['http://json-schema.org/draft-07/schema'];
+
+
+      // Compile the schema
+      const validate = ajv.validateSchema(obj);
+
+      // Check if the schema is valid
+      //  const isSchemaValid = validate(schemaDocument);
+
+      if (validate) {
+         console.log(`${file}: JSON Schema is valid!`);
+      } else {
+         console.error(`${file}: JSON Schema is not valid:`, ajv.errors);
+         process.exit(1);
+      }
+   } catch (error) {
+      console.error(`${file}: Error reading or parsing JSON Schema:`, error.message);
+      draft07();
+      // process.exit(1);
+   }
+   });
+}
+
+
+
+function draft07(){
+   const Ajv = require("ajv");
+   const ajv = new Ajv();
+
+   // Specify the path to the 'schemas' directory
+   const directoryPath = '../schemas';
+
+   // var json = globToRegExp("*.json");
+
+   // Read the files from the 'schemas' directory
+
+   fs.readdirSync(directoryPath).forEach(file => {
+   // Construct the full path to the JSON schema file
+   const filePath = path.join(directoryPath, file);
+
+   try {
+      // Read and parse the JSON schema
+      const fileContent = fs.readFileSync(filePath, 'utf8');
+      const obj = JSON.parse(fileContent);
+      
+      delete obj.definitions['http://json-schema.org/draft-04/schema'];
+      delete obj.definitions['http://json-schema.org/draft-07/schema'];
+
+
+      // Compile the schema
+      const validate = ajv.validateSchema(obj);
+
+      // Check if the schema is valid
+      //  const isSchemaValid = validate(schemaDocument);
+
+      if (validate) {
+         console.log(`${file}: JSON Schema is valid!`);
+      } else {
+         console.error(`${file}: JSON Schema is not valid:`, ajv.errors);
+         process.exit(1);
+      }
+   } catch (error) {
+      console.error(`${file}: Error reading or parsing JSON Schema:`, error.message);
+    //   draft04();
+      process.exit(1);
+   }
+   });
+
+}
+
+
+draft04();
+draft07();
