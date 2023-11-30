26ffcd0c637cff2eced0fa10f60685172d2f92d0
Jonas Lagoni <jonas-lt@live.dk>
2023-11-30 09:29:24 -1000

    feat!: add 3.0.0 validation support (#462)
    Co-authored-by: Sergio Moya <1083296+smoya@users.noreply.github.com>%0ACo-authored-by: asyncapi-bot <bot+chan@asyncapi.io>%0ACo-authored-by: asyncapi-bot-eve <bot+eve@asyncapi.io>%0ACo-authored-by: Fran Méndez <fmvilas@gmail.com>%0ACo-authored-by: Lukasz Gornicki <lpgornicki@gmail.com>%0ACo-authored-by: Maciej Urbańczyk <urbanczyk.maciej.95@gmail.com>

--- scripts/add-new-version.js
+++ scripts/add-new-version.js
@@ -1,3 +1,4 @@
+const path = require('path');
 /**
  * This script adds a new version of the spec with examples, by copying the latest one as baseline.
  */
@@ -23,6 +24,63 @@ function execute(command) {
   });
 }
 
+/**
+ * Add the new AsyncAPI schema object as values to schemaFormat
+ * 
+ * If a major version change, replaces all old AsyncAPI schemaFormat values with fresh ones
+ * if minor or fix, it add new ones
+ */
+function addNewSchemaVersion(newVersion, newVersionDir, latestVersion) {
+  const newSchemaFormats = [
+    `application/vnd.aai.asyncapi;version=${newVersion}`,
+    `application/vnd.aai.asyncapi+json;version=${newVersion}`,
+    `application/vnd.aai.asyncapi+yaml;version=${newVersion}`
+  ];
+  //Did the major version (first char) change from last to new version?
+  const isMajorVersionChange = newVersion.charAt(0) !== latestVersion.charAt(0);
+  const objFile = path.resolve(newVersionDir, 'multiFormatSchema.json');
+  const obj = require(objFile);
+
+  // Adapt all the MUST supported schema formats
+  let mustSupportedSchemaFormats = [] = obj?.else?.properties?.schemaFormat?.anyOf[1]?.enum;
+
+  //Add new version to the list of available schemaFormat values
+  if(mustSupportedSchemaFormats) {
+    if(isMajorVersionChange) {
+      //Remove all old AsyncAPI schema formats because we want a clean slate
+      mustSupportedSchemaFormats = mustSupportedSchemaFormats.filter((format) => !format.includes('application/vnd.aai.asyncapi'));
+    }
+    //Add new schema formats
+    mustSupportedSchemaFormats.push(...newSchemaFormats);
+    obj.else.properties.schemaFormat.anyOf[1].enum = mustSupportedSchemaFormats;
+  } else {
+    throw new Error("Could not find object to add schemaFormat values to");
+  }
+
+  //Make sure new versions apply the right schema
+  let enumsForValidatingSchema = [] = obj?.else?.allOf[1]?.if?.properties?.schemaFormat?.enum;
+  if(enumsForValidatingSchema) {
+    //Add new schema formats
+    enumsForValidatingSchema.push(...newSchemaFormats);
+    obj.else.allOf[1].if.properties.schemaFormat.enum = enumsForValidatingSchema;
+  } else {
+    throw new Error("Could not find location for schemaFormats that applies the AsyncAPI Schema object to the schema property");
+  }
+  
+  fs.writeFileSync(objFile, JSON.stringify(obj, null, 2));
+}
+
+/**
+ * Adapt the root title and .asyncapi property
+ */
+function adaptRootObject(newVersion, newVersionDir) {
+  const objFile = path.resolve(newVersionDir, 'asyncapi.json');
+  const obj = require(objFile);
+  obj.title = `AsyncAPI ${newVersion} schema.`;
+  obj.properties.asyncapi.const = newVersion;
+  fs.writeFileSync(objFile, JSON.stringify(obj, null, 2));
+}
+
 async function addNewVersion(newVersion) {
   const newVersionDir = `./definitions/${newVersion}`;
   const newExampleVersionDir = `./examples/${newVersion}`;
@@ -38,11 +96,17 @@ async function addNewVersion(newVersion) {
   await execute(`cp -R ./definitions/${latestVersion} ${newVersionDir}`);
 
   const latestExampleVersion = (await execute('ls -d ./examples/* | sort -V -r | head -1 | xargs -n 1 basename')).trim();
-  await execute(`cp -R ./definitions/${latestVersion} ${newVersionDir}`);
   await execute(`cp -R ./examples/${latestExampleVersion} ${newExampleVersionDir}`);
   
-  // Replace old version numbers with new
-  await execute(`find ${newVersionDir} -name '*.json' -exec sed -i '' "s+${latestVersion}+${newVersion}+g" {} +`);
+  // Replace $ref and $id paths such as `/3.0.0/` with new version (http://asyncapi.com/definitions/3.0.0/specificationExtension.json)
+  await execute(`find ${newVersionDir} -name '*.json' -exec sed -i '' \"s+\/${latestVersion}\/+\/${newVersion}\/+g\" {} +`);
+
+  // Replace .asyncapi version from old to new version
+  // Replace old version in title with new version
+  adaptRootObject(newVersion, newVersionDir);
+  
+  // Add new schemaFormat version entries
+  addNewSchemaVersion(newVersion, newVersionDir, latestVersion);
 
   console.log(`New version added to ${newVersionDir}`)
 }
