4a85696e71ad59b6f3c370714ea533fbd7811d0a
Pavel Bodiachevskii <PavelBodyachevskiy@gmail.com>
2024-05-22 03:29:21 +0400

    test(definitions): SCRAM-SHA-256
    https://github.com/asyncapi/spec-json-schemas/issues/539

--- test/definitions/3.0.0/security/index.js
+++ test/definitions/3.0.0/security/index.js
@@ -4,4 +4,5 @@ describe('Security Schemes', () => {
   require('./gssapi/gssapi.js');
   require('./openIdconnect/openIdconnect.js');
   require('./plain/plain.js');
+  require('./scramSha256/scramSha256.js');
 });
