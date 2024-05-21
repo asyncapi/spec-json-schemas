873295438e54a6c63227dbd9ccbab2186c080a87
Pavel Bodiachevskii <PavelBodyachevskiy@gmail.com>
2024-05-21 20:02:27 +0400

    test(definitions): Info
    check Info Json Schema

---
+++ test/definitions/3.0.0/info/info.js
@@ -0,0 +1,76 @@
+const Ajv = require('ajv');
+const assert = require('assert');
+const addFormats = require('ajv-formats');
+const fs = require('fs');
+
+const ajv = new Ajv({
+  jsonPointers: true,
+  allErrors: true,
+  schemaId: '$id',
+  logger: false,
+  validateFormats: true,
+  strict: false,
+});
+addFormats(ajv);
+
+const infoJsonSchema = require('../../../../definitions/3.0.0/info.json');
+const validator = ajv
+  .addMetaSchema(require('../../../../definitions/3.0.0/schema.json'))
+  .addSchema(require('../../../../definitions/3.0.0/infoExtensions.json'))
+  .addSchema(require('../../../../definitions/3.0.0/contact.json'))
+  .addSchema(require('../../../../definitions/3.0.0/license.json'))
+  .addSchema(require('../../../../examples/3.0.0/ReferenceObject.json'))
+  .addSchema(require('../../../../definitions/3.0.0/ReferenceObject.json'))
+  .addSchema(require('../../../../definitions/3.0.0/Reference.json'))
+  .addSchema(require('../../../../definitions/3.0.0/tag.json'))
+  .addSchema(require('../../../../definitions/3.0.0/externalDocs.json'))
+  .addSchema(require('../../../../definitions/3.0.0/specificationExtension.json'))
+  .addSchema(require('../../../../extensions/linkedin/0.1.0/schema.json'))
+  .addSchema(require('../../../../extensions/x/0.1.0/schema.json'))
+  .compile(infoJsonSchema);
+
+describe('Info', () => {
+  it('empty', () => {
+    const info = JSON.parse(fs.readFileSync(`${__dirname}/empty.json`, 'utf-8'));
+    const validationResult = validator(info);
+
+    assert(validationResult === false, 'Info with empty body is not valid');
+    assert(validator.errors[0].message === 'must have required property \'version\'');
+    assert(validator.errors[1].message === 'must have required property \'title\'');
+    assert(validator.errors.length === 2);
+  });
+
+  it('without required properties', () => {
+    const info = JSON.parse(fs.readFileSync(`${__dirname}/without required properties.json`, 'utf-8'));
+    const validationResult = validator(info);
+
+    assert(validationResult === false, 'Info is not valid without required properties');
+    assert(validator.errors[0].message === 'must have required property \'version\'');
+    assert(validator.errors[1].message === 'must have required property \'title\'');
+    assert(validator.errors.length === 2);
+  });
+
+  it('only required properties', () => {
+    const info = JSON.parse(fs.readFileSync(`${__dirname}/only required properties.json`, 'utf-8'));
+    const validationResult = validator(info);
+
+    assert(validationResult === true, 'Info is valid with only required properties');
+  });
+
+  it('extended', () => {
+    const info = JSON.parse(fs.readFileSync(`${__dirname}/extended.json`, 'utf-8'));
+    const validationResult = validator(info);
+
+    assert(validationResult === true, 'Info can be extended');
+  });
+
+  it('wrongly extended', () => {
+    const info = JSON.parse(fs.readFileSync(`${__dirname}/wrongly extended.json`, 'utf-8'));
+    const validationResult = validator(info);
+
+    assert(validationResult === false, 'Info is not valid when was wrongly extended');
+    assert(validator.errors[0].message === 'must NOT have additional properties');
+    assert(validator.errors[0].params.additionalProperty === 'ext-number');
+    assert(validator.errors.length === 1);
+  });
+});
--- test/index.js
+++ test/index.js
@@ -2,6 +2,10 @@ const assert = require('assert');
 const fs = require('fs');
 const path = require('path');
 
+describe('AsyncAPI: 3.0.0', () => {
+  require('./definitions/3.0.0/info/info.js');
+});
+
 describe('AsyncAPI', () => {
   it('should return an object', () => {
     const asyncapi = require('..');
@@ -13,21 +17,21 @@ describe('AsyncAPI', () => {
     const files = fs.readdirSync('schemas');
     files.forEach(file => {
       const fileName = path.parse(file).name;
-      
+
       if (skipFiles.includes(fileName)) return;
       const asyncapi = require('..');
 
       if (fileName.includes('-without-$id')) {
         const schemaName = fileName.replace('-without-$id', '');
-        assert(typeof asyncapi.schemasWithoutId[schemaName] === 'object', `Returned object does not contain ${schemaName}.`);  
+        assert(typeof asyncapi.schemasWithoutId[schemaName] === 'object', `Returned object does not contain ${schemaName}.`);
         const asyncapiVersion = require('..').schemasWithoutId[schemaName];
         const asyncapiSchema = require(`../schemas/${fileName}.json`);
-        assert.deepStrictEqual(asyncapiVersion, asyncapiSchema, `Returned object is not schema version ${schemaName}.`);  
+        assert.deepStrictEqual(asyncapiVersion, asyncapiSchema, `Returned object is not schema version ${schemaName}.`);
       } else {
         assert(typeof asyncapi.schemas[fileName] === 'object', `Returned object does not contain ${fileName}.`);
         const asyncapiVersion = require('..').schemas[fileName];
         const asyncapiSchema = require(`../schemas/${fileName}.json`);
-        assert.deepStrictEqual(asyncapiVersion, asyncapiSchema, `Returned object is not schema version ${fileName}.`);  
+        assert.deepStrictEqual(asyncapiVersion, asyncapiSchema, `Returned object is not schema version ${fileName}.`);
       }
     });
   });
