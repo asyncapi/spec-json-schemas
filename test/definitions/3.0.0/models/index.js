6187a47ebbaf493ba9acaa33a0e7c4290ff2d64f
Pavel Bodiachevskii <PavelBodyachevskiy@gmail.com>
2024-05-23 16:30:37 +0400

    test(definitions): regroup models
    https://github.com/asyncapi/spec-json-schemas/issues/539

--- test/definitions/3.0.0/models/index.js
+++ test/definitions/3.0.0/models/index.js
@@ -1,8 +1,5 @@
 describe('Models', () => {
-  require('./info/info.js');
-  require('./info extensions/info-extension.js');
-  require('./contact/contact.js');
-  require('./license/license.js');
+  require('./info');
   require('./reference/reference.js');
   require('./reference object/reference-object.js');
 });
---
+++ test/definitions/3.0.0/models/info/index.js
@@ -0,0 +1,5 @@
+describe('Info Object', () => {
+  require('./info.js');
+  require('./info extensions/info-extension.js');
+  require('./contact/contact.js');
+});
