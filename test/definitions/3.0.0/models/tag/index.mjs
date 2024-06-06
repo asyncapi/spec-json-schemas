ee748ca778aed2e25c3acd276354034bdd9f2f5b
Pavel Bodiachevskii <PavelBodyachevskiy@gmail.com>
2024-06-06 19:34:32 +0400

    test(definitions): Tag new tests + description
    https://github.com/asyncapi/spec-json-schemas/issues/539

--- test/definitions/3.0.0/models/tag/index.mjs
+++ test/definitions/3.0.0/models/tag/index.mjs
@@ -5,35 +5,35 @@ import path from 'path';
 const jsonSchema = require('@definitions/3.0.0/tag.json');
 
 describe('Tag', () => {
-  it('example', () => TestHelper.objectIsValid(
+  it('example is valid', () => TestHelper.objectIsValid(
     jsonSchema,
     path.resolve(__dirname, './example.json'),
   ));
 
-  it('empty', () => TestHelper.objectIsNotValid(
+  it('cannot be empty', () => TestHelper.objectIsNotValid(
     jsonSchema,
     path.resolve(__dirname, './empty.json'),
     ['must have required property \'name\'']
   ));
 
-  it('without required properties', () => TestHelper.objectIsNotValid(
+  it('is not valid without required properties', () => TestHelper.objectIsNotValid(
     jsonSchema,
     path.resolve(__dirname, './without required properties.json'),
     ['must have required property \'name\'']
   ));
 
-  it('only required properties', () => TestHelper.objectIsValid(
+  it('is valid with only required properties', () => TestHelper.objectIsValid(
     jsonSchema,
     path.resolve(__dirname, './only required properties.json')
   ));
 
-  it.skip('extended. Reason: schema doesn\'t check for extensions', () => TestHelper.objectIsValid(
+  it('is valid when is extended', () => TestHelper.objectIsValid(
     jsonSchema,
-    path.resolve(__dirname, '../../../extended.json')
+    path.resolve(__dirname, './extended.json')
   ));
 
-  it.skip('wrongly extended. Reason: schema doesn\'t check for extensions', () => TestHelper.objectIsValid(
+  it('is not valid when is wrongly extended', () => TestHelper.wronglyExtended(
     jsonSchema,
-    path.resolve(__dirname, '../../../wrongly extended.json')
+    path.resolve(__dirname, './wrongly extended.json')
   ));
 });
