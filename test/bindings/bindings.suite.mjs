079f853ac0827cbbe98cf999d2819ea165ffef65
Pavel Bodiachevskii <PavelBodyachevskiy@gmail.com>
2024-06-14 22:27:24 +0400

    test(definitions): use .each for solace tests
    https://github.com/asyncapi/spec-json-schemas/issues/539

--- test/bindings/bindings.suite.mjs
+++ test/bindings/bindings.suite.mjs
@@ -1,7 +1,6 @@
 import {describe} from 'vitest';
 
 describe('Bindings Test Suite', async () => {
-  await import('@test/bindings/solace/index.mjs');
   await import('@test/bindings/sqs/index.mjs');
   await import('@test/bindings/websockets/index.mjs');
 })
