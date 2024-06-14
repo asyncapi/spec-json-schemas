2f16869cb96b1610001a11801bb7e64382e182bf
Pavel Bodiachevskii <PavelBodyachevskiy@gmail.com>
2024-06-14 22:05:55 +0400

    test(definitions): use .each for nats tests
    https://github.com/asyncapi/spec-json-schemas/issues/539

--- test/bindings/nats/index.mjs
+++
@@ -1,7 +0,0 @@
-import {describe} from 'vitest';
-
-describe('NATS Test Suite', () => {
-  describe('0.1.0', async () => {
-    await import('./0.1.0/operation');
-  });
-})
--- vite.config.ts
+++ vite.config.ts
@@ -19,6 +19,7 @@ export default defineConfig({
         '**/test/bindings/jms/jms.test.mjs',
         '**/test/bindings/kafka/kafka.test.mjs',
         '**/test/bindings/mqtt/mqtt.test.mjs',
+        '**/test/bindings/nats/nats.test.mjs',
         '**/test/definitions/3.0.0/definitions.suite.mjs',
     ],
     reporters: ['verbose'] // https://vitest.dev/guide/reporters#custom-reporters
